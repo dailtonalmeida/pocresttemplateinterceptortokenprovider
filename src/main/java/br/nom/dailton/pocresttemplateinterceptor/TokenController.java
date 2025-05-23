93e69da894502673a7a7171e210b74d279c6ad5c
Dailton Santana de Almeida <dailton.almeida@gft.com>
2025-05-23 13:03:20 -0300

    tempo de expiracao vindo do servidor / 401 vindo da API

--- src/main/java/br/nom/dailton/pocresttemplateinterceptor/TokenController.java
+++ src/main/java/br/nom/dailton/pocresttemplateinterceptor/TokenController.java
@@ -3,20 +3,26 @@ package br.nom.dailton.pocresttemplateinterceptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
+import java.time.Clock;
+import java.time.Instant;
 
 @RestController
 @RequestMapping("/tokenprovider")
 public class TokenController {
     public record TokenRequest(String clientId, String clientSecret) {}
+    public record TokenResponse(String token, Instant expiresAt) {}
     private static final Logger log = LoggerFactory.getLogger(TokenController.class);
 
     @PostMapping(value = "", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Map<String, Object> provideToken(@RequestBody TokenRequest request) {
+    public TokenResponse provideToken(@RequestBody TokenRequest request) {
         log.atInfo().log("RECEIVED TOKEN REQUEST: {}", request);
-        return Map.of("token", "TOKEN_FROM_PROVIDER");
+        Instant expiresAt = Instant.now(Clock.systemUTC()).plusSeconds(4L);
+        return new TokenResponse("TOKEN_FROM_PROVIDER", expiresAt);
     }
 
 }
